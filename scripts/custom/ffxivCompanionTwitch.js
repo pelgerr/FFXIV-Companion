6487dfeae6118ca452152f940e5568f12fa8668f
Robert Pelger <4869909+onigiri070@users.noreply.github.com>
2019-08-09 18:01:56 -0600

    Implemented language files

--- scripts/custom/ffxivCompanionTwitch.js
+++ scripts/custom/ffxivCompanionTwitch.js
@@ -29,9 +29,11 @@
         if (regionArr.indexOf(String(args[0].toUpperCase())) !== -1) {
             region = String(args[0].toUpperCase());
             $.setIniDbString('regionTable', 'currentRegion', region);
-            $.say("Region successfully changed to " + region);
+            //$.say("Region successfully changed to " + region);
+            $.say($.lang.get('ffxivtwitch.region.success', region));
         } else {
-            $.say("Invalid region. Please use NA, EU, or JP.")
+            //$.say("Invalid region. Please use NA, EU, or JP.")
+            $.say($.lang.get('ffxivtwitch.region.invalid'));
         }
     }
 
@@ -80,7 +82,7 @@
          * Region command
          */
         if (command.equalsIgnoreCase('xivregion')) {
-            if (args.length < 1) {$.say("Region currently set to " + region); 
+            if (args.length < 1) {$.say($.lang.get('ffxivtwitch.region.current', region)); 
             return;
             } else {
                 regionSwitch(args);
